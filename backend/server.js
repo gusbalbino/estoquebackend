a02de9ca72756efb89d8e7a1d97f77da1ec54fa3
Gustavo André <gustjvo@gmail.com>
2026-04-21 19:03:57 -0300

    erro render corrigido

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dns = require('dns'); // <-- Adicionado para configurar o DNS internamente
+const dns = require('dns'); 
 require('dotenv').config();
-
 
 // Força o Node a usar o DNS do Google internamente, contornando bloqueios do Windows/Provedor
 dns.setServers(['8.8.8.8', '8.8.4.4']);
@@ -12,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Conexão atualizada forçando IPv4 (family: 4)
+
 mongoose.connect(process.env.MONGO_URI, {
     family: 4, 
     serverSelectionTimeoutMS: 5000 
@@ -20,9 +19,7 @@ mongoose.connect(process.env.MONGO_URI, {
     .then(() => console.log('⚡ Conectado com Sucesso ao Atlas!'))
     .catch(err => console.error('Erro ao conectar no Atlas:', err.message));
 
-// ==========================================
-// MÓDULO DE USUÁRIOS E LOGIN (ATUALIZADO)
-// ==========================================
+
 const UsuarioSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     senha: { type: String, required: true },
@@ -30,17 +27,16 @@ const UsuarioSchema = new mongoose.Schema({
 });
 const Usuario = mongoose.model('Usuario', UsuarioSchema);
 
-// Cria o Admin e o Gerente se não existirem
-// Cria ou Atualiza o Admin e o Gerente garantindo os cargos
+
 async function criarUsuariosPadrao() {
-    // Procura o admin. Se não achar, cria. Se achar, garante que o cargo é 'admin'
+    //admin
     await Usuario.findOneAndUpdate(
         { email: 'admin@eletronexo.com' }, 
         { senha: 'admineletronexo', cargo: 'admin' }, 
-        { upsert: true } // O upsert é a mágica: cria se não existir, atualiza se existir
+        { upsert: true } // cria se não existir atualiza se existir
     );
 
-    // Procura o gerente. Se não achar, cria. Se achar, garante que o cargo é 'gerente'
+    //gerente
     await Usuario.findOneAndUpdate(
         { email: 'gerente@eletronexo.com' }, 
         { senha: 'gerenteeletronexo', cargo: 'gerente' }, 
@@ -51,20 +47,20 @@ async function criarUsuariosPadrao() {
 }
 criarUsuariosPadrao();
 
-// Rota de Login (Agora devolve o cargo do usuário)
+// Rota de Login 
 app.post('/api/login', async (req, res) => {
     const { email, senha } = req.body;
     const usuario = await Usuario.findOne({ email, senha });
     
     if (usuario) {
-        // Agora mandamos o cargo junto com a resposta de sucesso!
+        
         res.json({ sucesso: true, mensagem: 'Bem-vindo!', cargo: usuario.cargo });
     } else {
         res.status(401).json({ sucesso: false, erro: 'Usuário ou senha incorretos' });
     }
 });
 
-// Rota SECRETA para o Admin cadastrar novos usuários (Você já tem essa)
+// Rota SECRETA para o admin cadastrar novos usuários
 app.post('/api/usuarios', async (req, res) => {
     try {
         const novo = new Usuario(req.body);
@@ -75,9 +71,9 @@ app.post('/api/usuarios', async (req, res) => {
     }
 });
 
-// 👇 NOVA ROTA: Buscar todos os usuários (sem mostrar a senha!)
+// Buscar todos os usuários
 app.get('/api/usuarios', async (req, res) => {
-    // O ".select('-senha')" garante que o banco não envie as senhas para o frontend
+   
     const usuarios = await Usuario.find().select('-senha');
     res.json(usuarios);
 });
@@ -139,4 +135,8 @@ app.get('/api/historico', async (req, res) => {
     res.json(hist);
 });
 
-app.listen(3001, () => console.log('API rodando na porta 3001'));
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(` API rodando na porta ${PORT}`);
+});
